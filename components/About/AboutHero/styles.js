e2146f917725863431c94dd77189390c44130e83
StarDev0011 <ondre.williams.dev@gmail.com>
2022-02-18 13:22:37 +0800

    About page updated

--- components/About/AboutHero/styles.js
+++ components/About/AboutHero/styles.js
@@ -33,23 +33,35 @@ export const AboutTitle = styled.h2`
 export const AboutSubtitle = styled.h4`
   text-align: left;
   font-size: 4rem;
+  width: 80%;
+  margin-left: auto;
+  margin-right: auto;
   font-family: ${(props) => props.theme.fonts.headingFont};
   font-weight: bold;
+  @media (max-width: 465px) {
+    width: 100%;
+  }
 `;
 export const AboutParagraph = styled.p`
   font-size: 24px;
   text-align: left;
   margin-top: 2.4rem;
+  width: 80%;
+  margin-left: auto;
+  margin-right: auto;
   line-height: 1.8;
   @media (max-width: 465px) {
     font-size:18px;
+    width: 100%;
   }
 `;
 export const AboutVideoWrapper = styled.div`
   margin-top: 150px;
+  margin-bottom: 250px;
   width: 100%;
   height: 550px;
   @media (max-width: 465px) {
     margin-top: 50px;
+    margin-bottom: 50px;
   }
 `;
