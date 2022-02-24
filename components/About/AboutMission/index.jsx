2bab752e2b7eba29b346f0130dd8ee5b202bc93b
StarDev0011 <ondre.williams.dev@gmail.com>
2022-02-25 04:04:28 +0800

    about page updated

--- components/About/AboutMission/index.jsx
+++ components/About/AboutMission/index.jsx
@@ -7,6 +7,7 @@ import {
 
 import React from 'react'
 import { useInView } from 'react-intersection-observer'
+import { SiteContainer } from '../../../atoms/SiteContainer'
 
 const AboutMission = () => {
   const { ref, inView } = useInView({
@@ -15,22 +16,24 @@ const AboutMission = () => {
   })
 
   return (
-    <AboutMissionWrapper>
-      <SiteImageWrapper>
-        <img alt="L'Dezen image" src="/images/mission-img1.jpg" />
-      </SiteImageWrapper>
-      <AboutMissionContainer ref={ref}>
-        <AboutMissionData>
-          <h4>Mission</h4>
-          <h3 className={inView + '_discover'}>Discovery. Redefined.</h3>
-          <p>Curate jewelery from artists around the world</p>
-          <br />
-          <p>Onboard the next wave of NFT collectors</p>
-          <br />
-          <p>Transform the traditional shopping experience</p>
-        </AboutMissionData>
-      </AboutMissionContainer>
-    </AboutMissionWrapper>
+    <SiteContainer>
+      <AboutMissionWrapper>
+        <SiteImageWrapper>
+          <img alt="L'Dezen image" src="/images/mission-img1.jpg" />
+        </SiteImageWrapper>
+        <AboutMissionContainer ref={ref}>
+          <AboutMissionData>
+            <h4>Mission</h4>
+            <h3 className={inView + '_discover'}>Discovery. Redefined.</h3>
+            <p>Curate jewelery from artists around the world</p>
+            <br />
+            <p>Onboard the next wave of NFT collectors</p>
+            <br />
+            <p>Transform the traditional shopping experience</p>
+          </AboutMissionData>
+        </AboutMissionContainer>
+      </AboutMissionWrapper>
+    </SiteContainer>
   )
 }
 
--- components/About/AboutVision/index.jsx
+++ components/About/AboutVision/index.jsx
@@ -5,6 +5,7 @@ import {
   VisionWrapper,
 } from './styles'
 import { useInView } from 'react-intersection-observer'
+import { SiteContainer } from '../../../atoms/SiteContainer'
 
 const AboutVision = () => {
   const { ref, inView } = useInView({
@@ -12,22 +13,24 @@ const AboutVision = () => {
     initialInView: true,
   })
   return (
-    <VisionWrapper>
-      <VisionContainer ref={ref}>
-        <VisionData>
-          <h4>Vision</h4>
-          <h3 className={inView + '_luxury'}>Luxury. Transformed.</h3>
-          <p>Cross the line between digital & physical worlds</p>
-          <br />
-          <p>Collect jewelry in the form of asset backed NFTs</p>
-          <br />
-          <p>Empower creative culture</p>
-        </VisionData>
-      </VisionContainer>
-      <SiteImageWrapper>
-        <img alt="vision image" src="/images/vision-about1.png" />
-      </SiteImageWrapper>
-    </VisionWrapper>
+    <SiteContainer>
+      <VisionWrapper>
+        <VisionContainer ref={ref}>
+          <VisionData>
+            <h4>Vision</h4>
+            <h3 className={inView + '_luxury'}>Luxury. Transformed.</h3>
+            <p>Cross the line between digital & physical worlds</p>
+            <br />
+            <p>Collect jewelry in the form of asset backed NFTs</p>
+            <br />
+            <p>Empower creative culture</p>
+          </VisionData>
+        </VisionContainer>
+        <SiteImageWrapper>
+          <img alt="vision image" src="/images/vision-about1.png" />
+        </SiteImageWrapper>
+      </VisionWrapper>
+    </SiteContainer>
   )
 }
 
