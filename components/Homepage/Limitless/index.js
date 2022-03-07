061832e3afddf8531f36954390e10102ffb0f44b
mitesh pandey <excelrock_mitesh@yahoo.com>
2022-03-07 22:22:05 +0545

    limitless index changed

--- components/Homepage/Limitless/index.js
+++ components/Homepage/Limitless/index.js
@@ -7,7 +7,55 @@ import {
   LimitlessWrapper,
   SiteImageWrapper,
   LimitlessButtonContainer,
+  FaqWrapper,
 } from './styles'
+import Faq from 'react-faq-component'
+
+const faqData = {
+  title: '',
+  rows: [
+    {
+      title: 'Digital',
+      content: `        
+          <li>3D Rendered Animation</li> 
+          <li>Custom Digital Rendering of the ‘Limitless’ Earrings on any PFP NFT you own</li>        
+        `,
+    },
+    {
+      title: 'Physical',
+      content: `       
+          <li>Physical version of the ‘Limitless’ Pair of Earrings, redeemable on digitalxtwin.io</li>
+          <li>These earrings are one of a kind and exclusive to the Digital Twin auction</li>
+          <li>L’Dezen jewelry is made in 18k Gold with conflict-free diamonds</li>                
+        `,
+    },
+    {
+      title: 'Details',
+      content: `        
+          <li>Diamonds Total Weight: 2.27 carat</li>
+          <li>18K Gold, Total Weight: 11.66 grams</li>
+          <li>Earrings Length: 5.2cm</li>
+          <li>Post fastening for pierced ears</li>
+          <li>Minting: This NFT was minted by L’Dezen on Ethereum </li>
+          <li>Bonus: Physical redemption includes 10% gift card on a future L’Dezen purchase</li>        
+      `,
+    },
+  ],
+}
+
+const faqStyles = {
+  // bgColor: 'white',
+  titleTextColor: '#333',
+  rowTitleColor: '#333',
+  rowContentColor: 'grey',
+  // arrowColor: "red",
+}
+
+const faqConfig = {
+  // animate: true,
+  // arrowIcon: "V",
+  // tabFocus: true
+}
 
 const Limitless = () => {
   return (
@@ -27,6 +75,9 @@ const Limitless = () => {
                 <p>{"L'Dezen by Payal Shah"}</p>
               </div>
               <br />
+              <FaqWrapper>
+                <Faq data={faqData} styles={faqStyles} config={faqConfig} />
+              </FaqWrapper>
               <div className="external_links">
                 <p>
                   <b>View us on:</b>
@@ -37,7 +88,7 @@ const Limitless = () => {
                   rel="noreferrer"
                 >
                   <img
-                    src="/images/etherscan-logo.png"
+                    src="/images/etherscan-logo.jpeg"
                     height="60px"
                     width="60px"
                     style={{ padding: '10px' }}
@@ -81,7 +132,7 @@ const Limitless = () => {
         <LimitlessButtonContainer>
           <a
             className="auction_btn"
-            href="token/0x8aDd76A5c38da958dfFF9A58DdE51798d03C5ef9/1"
+            href="token/0xD391646321ccf7938821a01d169DeA6922AEDBba/6"
           >
             Enter Auction
           </a>
