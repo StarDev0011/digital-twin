3e1c68e6668a2939798736ab0a7319c829770032
mitesh pandey <excelrock_mitesh@yahoo.com>
2022-03-08 08:31:40 +0545

    added the token query part

--- pages/api/getUserData.ts
+++ pages/api/getUserData.ts
@@ -1,12 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-import redis from '../../lib/redis'
+// import redis from '../../lib/redis'
 
 export default async function getAllFeatures(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const data = (await redis.hvals('shipping')).map((entry) => JSON.parse(entry))
+  // const data = (await redis.hvals('shipping')).map((entry) => JSON.parse(entry))
 
-  res.status(200).json({ data })
+  // res.status(200).json({ data })
+  res.status(200).json({ req })
 }
--- pages/api/sendUserData.ts
+++ pages/api/sendUserData.ts
@@ -1,47 +1,47 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import { NextApiRequest, NextApiResponse } from 'next'
-import pinataSDK from '@pinata/sdk'
-const pinata = pinataSDK(
-  '7aec58df8140ec00c355',
-  'd458ce746f81f57d6b0e35442a39b427da61af47a0f1c3f753c78db650ff0e04'
-)
-import redis from '../../lib/redis'
-import { v4 as uuidv4 } from 'uuid'
-async function uploadJsonToDecentralizedStorage(data) {
-  // function that uploads buffer to decentralized storage
-  // and returns url of uploaded file from a gateway.
-  // const readableStreamForFile = fs.createReadStream(url);
-  return new Promise((resolve, reject) => {
-    pinata
-      .pinJSONToIPFS(data)
-      .then((result) => {
-        //handle results here
-        // console.log(result);
-        resolve(`https://gateway.pinata.cloud/ipfs/${result.IpfsHash}`)
-      })
-      .catch((err) => {
-        //handle error here
-        // console.log(err);
-        reject(err)
-      })
-  })
-}
+// import { NextApiRequest, NextApiResponse } from 'next'
+// import pinataSDK from '@pinata/sdk'
+// const pinata = pinataSDK(
+//   '7aec58df8140ec00c355',
+//   'd458ce746f81f57d6b0e35442a39b427da61af47a0f1c3f753c78db650ff0e04'
+// )
+// import redis from '../../lib/redis'
+// import { v4 as uuidv4 } from 'uuid'
+// async function uploadJsonToDecentralizedStorage(data) {
+//   // function that uploads buffer to decentralized storage
+//   // and returns url of uploaded file from a gateway.
+//   // const readableStreamForFile = fs.createReadStream(url);
+//   return new Promise((resolve, reject) => {
+//     pinata
+//       .pinJSONToIPFS(data)
+//       .then((result) => {
+//         //handle results here
+//         // console.log(result);
+//         resolve(`https://gateway.pinata.cloud/ipfs/${result.IpfsHash}`)
+//       })
+//       .catch((err) => {
+//         //handle error here
+//         // console.log(err);
+//         reject(err)
+//       })
+//   })
+// }
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    const url = await uploadJsonToDecentralizedStorage(req.body)
-    const id = uuidv4()
-    await redis.hset('shipping', id, JSON.stringify(req.body))
-    res.status(200).json({
-      body: 'success',
-      url,
-    })
+// const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+//   try {
+//     const url = await uploadJsonToDecentralizedStorage(req.body)
+//     const id = uuidv4()
+//     await redis.hset('shipping', id, JSON.stringify(req.body))
+//     res.status(200).json({
+//       body: 'success',
+//       url,
+//     })
 
-    // res.status(200).json({ url })
-  } catch (err) {
-    res.status(404).json({ err })
-  }
-}
+//     // res.status(200).json({ url })
+//   } catch (err) {
+//     res.status(404).json({ err })
+//   }
+// }
 
-export default handler
+// export default handler
