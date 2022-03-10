0324326bd96866eff28d5fea528e72d483f9ce84
mitesh pandey <excelrock_mitesh@yahoo.com>
2022-03-10 22:36:22 +0545

    updating the auction start time

--- pages/token/[contract]/[id].tsx
+++ pages/token/[contract]/[id].tsx
@@ -374,7 +374,7 @@ export default function Piece({ initialData }: PieceProps) {
                   </div>
                   <div className="start_date">
                     <p>AUCTION STARTS ON</p>
-                    <h2>March 10, 2022</h2>
+                    <h2>March 10, 2022 at 8pm EST</h2>
                   </div>
                   <div className="bid_btn">
                     <Button>Coming Soon</Button>
