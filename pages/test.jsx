94a32f1e367af5fba15fc0b0a75a426361ebbf85
sumitmydevit <sumit@mydevitsolutions.com>
2022-03-10 11:25:44 +0530

    added model to test route

--- pages/test.jsx
+++ pages/test.jsx
@@ -1,9 +1,17 @@
-import DetailBox from '../components/RedeemPhysical/DetailBox'
+//import DetailBox from '../components/RedeemPhysical/DetailBox'
 import Layout from '../components/Layout'
+import SiteModal from '../atoms/SiteModal'
 const Test = () => {
+
+  const modalData = {
+    type: 'success',
+    title: 'Success',
+    message: 'Congrats, we received your shipping details. You will receive a confirmation email once your item has shipped.'
+  }
   return (
     <Layout>
-      <DetailBox />
+      {/* <DetailBox /> */}
+      <SiteModal data={modalData}/>
     </Layout>
   )
 }
